4cd4a22a6991fea2e7dd09960775787bbfef5523
Metalscreame <mantilocal@gmail.com>
2018-01-11 22:50:48 +0200

    bugfix

--- src/main/java/com/compay/service/TokenService.java
+++ src/main/java/com/compay/service/TokenService.java
@@ -1,9 +1,7 @@
 package com.compay.service;
 
 import com.compay.entity.Token;
-import javafx.scene.paint.Stop;
 
-import java.util.List;
 
 public interface TokenService {
     Token findByToken(String token);
