60dbee28bf462721ccd8a4e49fe03c48a9ef6bae
Roman Kosiy Metalscreame <mantilocal@gmail.com>
2018-01-17 21:16:10 +0200

    bug fixing

--- src/main/java/com/compay/controller/securityContollers/RegistrationController.java
+++ src/main/java/com/compay/controller/securityContollers/RegistrationController.java
@@ -1,24 +1,18 @@
 package com.compay.controller.securityContollers;
 
 
-import com.compay.entity.Token;
 import com.compay.entity.User;
-import com.compay.json.jsonReceive.login.PersonToLoginEntity;
+
 import com.compay.json.jsonReceive.register.PersonToRegisterEntity;
 import com.compay.service.MailService;
 import com.compay.service.TokenService;
 import com.compay.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.sqlite.SQLiteException;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.sql.SQLException;
 
 @Controller
 
@@ -29,18 +23,16 @@ public class RegistrationController {
     @Autowired
     private MailService mailSender;
 
-    @Autowired
-    private TokenService tokenService;
 
     @RequestMapping(value = "/auth/register",method = RequestMethod.POST,produces = "text/plain;charset=UTF-8")//TODO change to post
     @ResponseBody
     public String registrationPage(@RequestHeader(value = "Content-Type") String contentType,
                                    @RequestBody String body,
-                                   HttpServletResponse response) throws IOException {
-        User user = new User();
-        PersonToRegisterEntity newUser = new ObjectMapper().readValue(body, PersonToRegisterEntity.class);
-
+                                   HttpServletResponse response) {
        try {
+           User user = new User();
+           PersonToRegisterEntity newUser = new ObjectMapper().readValue(body, PersonToRegisterEntity.class);
+
            //check for unique email
            User checkerUser = service.findByEmail(newUser.getEmail());
            if(checkerUser!=null){
