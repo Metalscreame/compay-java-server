d40bbb93f3573f2fc62017849cc8a32095e8b760
Roman Kosiy Metalscreame <mantilocal@gmail.com>
2018-01-20 09:40:10 +0200

    bug fix with user passwords after changing the encoder

--- src/main/java/com/compay/controller/ResponseControllers/CalculationMethods.java
+++ src/main/java/com/compay/controller/ResponseControllers/CalculationMethods.java
@@ -27,12 +27,12 @@ public class CalculationMethods {
             } else throw new AuthException();
             String result = null;
 
-            result ="[{\"id\":\"fixSum\", \"name\":\"Фиксированная сумма\"},{\"id\":\"fixFormula\",\"name\":\"Фиксированная формула\"},{\"id\":\"counter\",\"name\":\"По счетчику\"},{\"id\":\"counterScale\",\"name\":\"По счетчику со шкалой\"},{\"id\":\"manual\",\"name\":\"Ручной ввод суммы\"}]";
+            result = "[{\"id\":\"fixSum\", \"name\":\"Фиксированная сумма\"},{\"id\":\"fixFormula\",\"name\":\"Фиксированная формула\"},{\"id\":\"counter\",\"name\":\"По счетчику\"},{\"id\":\"counterScale\",\"name\":\"По счетчику со шкалой\"},{\"id\":\"manual\",\"name\":\"Ручной ввод суммы\"}]";
             response.setStatus(200);
             response.setHeader("headers", "{\"Content-Type':\"application/json\"}");
-            return  result;
+            return result;
 
-        }catch (AuthException e){
+        } catch (AuthException e) {
             response.setStatus(401);
             response.setHeader("headers", "{\"Content-Type\":\"application/json\"}");
             return "{\"message\": \"Unauthorized\"}";
@@ -42,19 +42,19 @@ public class CalculationMethods {
     @RequestMapping(value = "/fixFormulas", method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
     @ResponseBody
     public String responseBody2(@RequestHeader(value = "Content-Type") String type,
-                               @RequestHeader(value = "Authorization") String authToken,
-                               HttpServletResponse response) throws JsonProcessingException, ParseException {
+                                @RequestHeader(value = "Authorization") String authToken,
+                                HttpServletResponse response) throws JsonProcessingException, ParseException {
         try {
             if (tokenService.authChek(authToken)) {
             } else throw new AuthException();
             String result = null;
 
-            result ="[{\"formulaId\":\"rate_livingSpace\",\"formula\":\" [Тариф] х [Жилая площадь]\",\"attrs\":{\"rate\":{\"name\":\"Тариф\",\"val\":\"\",\"units\":\"(грн/м2)/мес\"},\"livingSpace\": {     \"name\":\"Жилая площадь\",\"val\":\"\",\"units\":\"м2\"}}},{\"formulaId\":\"rate_coeff\",\"formula\":\" [Тариф] х [Коэффициент]\",\"attrs\":{\"rate\":{\"name\":\"Тариф\",\"val\":\"\",\"units\":\"(грн/м2)/мес\"},\"coeff\": {\"name\":\"Коэффициент\",\"val\":\"\",\"units\":\"\"}}}]";
+            result = "[{\"formulaId\":\"rate_livingSpace\",\"formula\":\" [Тариф] х [Жилая площадь]\",\"attrs\":{\"rate\":{\"name\":\"Тариф\",\"val\":\"\",\"units\":\"(грн/м2)/мес\"},\"livingSpace\": {     \"name\":\"Жилая площадь\",\"val\":\"\",\"units\":\"м2\"}}},{\"formulaId\":\"rate_coeff\",\"formula\":\" [Тариф] х [Коэффициент]\",\"attrs\":{\"rate\":{\"name\":\"Тариф\",\"val\":\"\",\"units\":\"(грн/м2)/мес\"},\"coeff\": {\"name\":\"Коэффициент\",\"val\":\"\",\"units\":\"\"}}}]";
             response.setStatus(200);
             response.setHeader("headers", "{\"Content-Type':\"application/json\"}");
-            return  result;
+            return result;
 
-        }catch (AuthException e){
+        } catch (AuthException e) {
             response.setStatus(401);
             response.setHeader("headers", "{\"Content-Type\":\"application/json\"}");
             return "{\"message\": \"Unauthorized\"}";
