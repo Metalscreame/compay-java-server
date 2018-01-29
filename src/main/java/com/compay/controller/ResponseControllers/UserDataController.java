d54ca4a1af7902a559c77ee76b73a0fbda78b21a
1-ПК\1 <anatolian999@gmail.com>
2018-01-29 21:51:34 +0300

    userData

---
+++ src/main/java/com/compay/controller/ResponseControllers/UserDataController.java
@@ -0,0 +1,66 @@
+package com.compay.controller.ResponseControllers;
+
+import com.compay.entity.Adress;
+import com.compay.entity.User;
+import com.compay.exception.AuthException;
+import com.compay.service.AdressService;
+import com.compay.service.TokenService;
+import com.compay.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class UserDataController {
+    @Autowired
+    private TokenService tokenService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private AdressService adressService;
+    @RequestMapping(value = "/userData", method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
+    @ResponseBody
+    public String responseBody(@RequestHeader(value = "Authorization") String authToken,
+                               HttpServletResponse response) throws AuthException, JsonProcessingException {
+        String res = "";
+
+
+        if (!tokenService.authChek(authToken)) {
+            response.setStatus(401);
+
+            ObjectNode error = new ObjectMapper().createObjectNode();
+            error.put("message","Unauthorized");
+            res = error.toString();
+        }
+        else {
+            User usrByToken = tokenService.findByToken(authToken).getUser();
+            Adress findedAdress = adressService.findDefaultAdressByUsrId(usrByToken.getId());
+
+            ObjectNode rootNode = new ObjectMapper().createObjectNode();
+            rootNode.put("isAdmin",usrByToken.getRole().equals("admin"));
+
+            ObjectNode adressNode = new ObjectMapper().createObjectNode();
+            adressNode.put("id",findedAdress.getId());
+            adressNode.put("name",findedAdress.getType()+", "+ findedAdress.getStreet()+" "+
+                    findedAdress.getHouseNumber()+"/"+findedAdress.getAppartmentNumber());
+            adressNode.put("objectDefault",findedAdress.getObjectDefault());
+
+            rootNode.put("currentObject",adressNode);
+
+            res  = rootNode.toString();
+
+            response.setStatus(200);
+            response.setHeader("headers", "{\"Content-Type\":\"application/json\"}");
+
+        }
+        return res;
+    }
+}
--- src/main/java/com/compay/repository/AdressRepository.java
+++ src/main/java/com/compay/repository/AdressRepository.java
@@ -4,6 +4,8 @@ import com.compay.entity.Adress;
 import com.compay.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
@@ -15,6 +17,9 @@ public interface AdressRepository extends JpaRepository<Adress, Integer> {
     @Transactional
     @Modifying
     void deleteById(int id);
+
+    @Query(value = "select a from Adress a where a.user.id= :userId and a.objectDefault=true")
+    List<Adress> findDefaultAdressByUsrId(@Param("userId") int userId);
 
 
 }
