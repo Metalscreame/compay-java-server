6d7d637843c9e8333402af56542e5d663895177a
Roman Kosiy Metalscreame <mantilocal@gmail.com>
2018-02-22 21:02:24 +0200

    changed idea, when there is no date - add

--- src/main/java/com/compay/repository/DefaultScalesRepository.java
+++ src/main/java/com/compay/repository/DefaultScalesRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 @Repository
 public interface DefaultScalesRepository extends JpaRepository<DefaultScales, Integer> {
     List<DefaultScales> findAllByDefaultRates(DefaultRates defaultRates);
+    List<DefaultScales> findAllByDefaultRates_Id(int id);
 }
